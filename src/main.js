657452d77517b797f9dc510d4600ef22475da4e6
Knight-Solace <126420167+Knight-Solace@users.noreply.github.com>
2023-02-26 21:39:51 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+Hooks.on("beavers-system-interface.init", async function () {
+    beaversSystemInterface.addModule("bsa-swade");
+});
+
+Hooks.once("beavers-system-interface.ready", async function () {
+
+}
+
 import {Swade} from "./Swade.js";
 
 Hooks.on("beavers-system-interface.init", async function(){
