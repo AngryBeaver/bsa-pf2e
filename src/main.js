e1e6b2f5c70e0efb3e55f3a0131e9d34393ebcc7
Knight-Solace <126420167+Knight-Solace@users.noreply.github.com>
2023-02-26 20:40:25 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {Swade} from "./Swade.js";
 
 Hooks.on("beavers-system-interface.init", async function(){
-    beaversSystemInterface.register(new Swade());
+    beaversSystemInterface.addModule("beavers-crafting");
 });
